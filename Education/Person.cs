0b77680f58d9c3c08b08bcda26e9c389ecbadbcc
Martin Kierkegaard <mark@easj.dk>
2017-10-11 13:23:44 +0200

    tilføjet id til Person klassen

--- Education/Person.cs
+++ Education/Person.cs
@@ -8,10 +8,10 @@ namespace Education
 {
     public class Person
     {
-
         public string SchoolClassId { get; set; }
         public string Name { get; set; }
         public int MobileNo { get; set; }
+        public int Id { get; set; }
 
 
     }
